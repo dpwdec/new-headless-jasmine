51cc5c38115b40e39798a834c07c496495b8b6de
dpwdec <declankolakowski@gmail.com>
2020-05-15 13:34:09 +0100

    added testing integration with Travis

--- test/mySpec.js
+++ test/mySpec.js
@@ -2,4 +2,8 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+
+  it('fails a test', function() {
+    expect(false).toBe(false);
+  });
 });
